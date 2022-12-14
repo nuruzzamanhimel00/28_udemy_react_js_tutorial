371df755370916cf1943a5d8ad7852afcc677120
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-12-15 03:14:13 +0600

    done

--- reactjs_complete_guide/src/Components/ExpenceItem.js
+++ reactjs_complete_guide/src/Components/ExpenceItem.js
@@ -4,7 +4,15 @@ function ExpenceItem(props){
 
     return (
             <div className='expence-item'>
-                <div className="expence-item__date">{props.date.toDateString()}</div>
+                {/* <div className="expence-item__date">{props.date.toDateString()}</div> */}
+                <div className="expence-item__date">
+                    <div className='expence-item__date__item'>
+                        <div>{props.date.toLocaleDateString('en-us',{ month: 'short'})}</div>
+                        <div>{props.date.toLocaleDateString('en-us',{   year: 'numeric'})}</div>
+                        <div>{props.date.toLocaleDateString('en-us',{   day: 'numeric'})}</div>
+                    </div>
+                   
+                </div>
                 <div className='expence-item__description'>
                     <h2>{props.title}</h2>
                     <div className='expence-item__price'>${props.amount}</div>
