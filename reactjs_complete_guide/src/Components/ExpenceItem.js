26f134f1e0aa3e9a69b2b676af54709efc7a84f2
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-12-14 23:16:41 +0600

    done

--- reactjs_complete_guide/src/Components/ExpenceItem.js
+++ reactjs_complete_guide/src/Components/ExpenceItem.js
@@ -1,15 +1,20 @@
 import './ExpenceItem.css'
 
 function ExpenceItem(){
-return (
-        <div className='expence-item'>
-            <div className="expence-item__date">March 25,2021</div>
-            <div className='expence-item__description'>
-                <h2>My car</h2>
-                <div className='expence-item__price'>$200</div>
+
+    const expenceDate = new Date(2018, 11, 24);
+    const expenceTitle = "My car";
+    const expencePrice = '$200';
+    // console.log(expenceDate);
+    return (
+            <div className='expence-item'>
+                <div className="expence-item__date">{expenceDate.toDateString()}</div>
+                <div className='expence-item__description'>
+                    <h2>{expenceTitle}</h2>
+                    <div className='expence-item__price'>{expencePrice}</div>
+                </div>
             </div>
-        </div>
-    );
+        );
 }
 
 export default ExpenceItem;
