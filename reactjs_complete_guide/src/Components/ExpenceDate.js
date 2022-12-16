862d7407da11f5c0974c31046786903a0a611057
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-12-16 12:21:47 +0600

    done

---
+++ reactjs_complete_guide/src/Components/ExpenceDate.js
@@ -0,0 +1,17 @@
+import './ExpenceItem.css'
+
+function ExpenceDate(props){
+    const month = props.date.toLocaleDateString('en-us',{ month: 'short'});
+    const year = props.date.toLocaleDateString('en-us',{ year: 'numeric'});
+    const day = props.date.toLocaleDateString('en-us',{ day: 'numeric'});
+
+    return (
+            <div className='expence-item__date__item'>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+        );
+}
+
+export default ExpenceDate;
--- reactjs_complete_guide/src/Components/ExpenceItem.js
+++ reactjs_complete_guide/src/Components/ExpenceItem.js
@@ -1,16 +1,14 @@
 import './ExpenceItem.css'
 
-function ExpenceItem(props){
+import ExpanceDate from './ExpenceDate.js'
 
+function ExpenceItem(props){
+ 
     return (
             <div className='expence-item'>
                 {/* <div className="expence-item__date">{props.date.toDateString()}</div> */}
                 <div className="expence-item__date">
-                    <div className='expence-item__date__item'>
-                        <div>{props.date.toLocaleDateString('en-us',{ month: 'short'})}</div>
-                        <div>{props.date.toLocaleDateString('en-us',{   year: 'numeric'})}</div>
-                        <div>{props.date.toLocaleDateString('en-us',{   day: 'numeric'})}</div>
-                    </div>
+                    <ExpanceDate date={props.date} />
                    
                 </div>
                 <div className='expence-item__description'>
