bafe2e13cedf4307c12ff58269e447caccbda656
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-01-02 23:17:48 +0600

    done

--- reactjs_complete_guide/src/Components/NewExpence/ExpenceForm.js
+++ reactjs_complete_guide/src/Components/NewExpence/ExpenceForm.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const ExpenceForm = () =>{
 
+    // const [enterTitle, setEnterTitle] = useState('');
+    // const [enterAmount, setEnterAmount] = useState('');
+    // const [enterDate, setEnterDate] = useState('');
+
+    const [useInput, setUseInput] = useState({
+        enterTitle : '',
+        enterAmount: '',
+        enterDate: ''
+    });
+
     const onTitleChangeHandler = (event) =>{
-        console.log(event.target.value);
+        setUseInput({
+            ...useInput,
+            enterTitle: event.target.value
+        });
+        // setEnterTitle(event.target.value);
+        // console.log(event.target.value);
+    }
+
+    const onAmountChangeHandler = (event) =>{
+        setUseInput({
+            ...useInput,
+            enterAmount: event.target.value
+        });
+        // setEnterAmount(event.target.value);
+    }
+
+    const onDateChangeHandler = (event) =>{
+        setUseInput({
+            ...useInput,
+            enterDate: event.target.value
+        });
+        // setEnterDate(event.target.value);
     }
 
     return (
@@ -15,11 +46,11 @@ const ExpenceForm = () =>{
                 </div>
                 <div className="form-group col-md-6">
                     <label for="amount">Amount</label>
-                    <input type="number" className="form-control" min='0.01' step="0.01" /> 
+                    <input type="number" onChange={onAmountChangeHandler} className="form-control" min='0.01' step="0.01" /> 
                 </div>
                 <div className="form-group col-md-6">
                     <label for="amount">Date</label>
-                    <input type="date" className="form-control" min='2022-12-31' max="2025-12-31" /> 
+                    <input type="date" onChange={onDateChangeHandler} className="form-control" min='2022-12-31' max="2025-12-31" /> 
                     <button type="submit" className="btn btn-primary mt-3">Add Exponce</button>
                 </div>
                 
