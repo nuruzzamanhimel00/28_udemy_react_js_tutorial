92ebf618227d5e2d7082a22d8ed0cb9cebf2e443
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-03-08 21:09:42 +0600

    done

--- 06.1_startat_style_react_comp_again/src/components/UI/Button.js
+++ 06.1_startat_style_react_comp_again/src/components/UI/Button.js
@@ -14,6 +14,10 @@ const Button = styled.button`
     background: #8b005d;
     width: auto;
 
+    @media(max-width: 768px){
+        width: 100%;
+    }
+
     &:hover{
         background: rgb(213, 0, 143);
     }
